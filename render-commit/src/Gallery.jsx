0cbefaa94d639a7e23c216c3773fe14b41d5a588
Mohammad-Taizul-Islam <m.taizul1045@gmail.com>
2024-02-19 12:16:08 +0600

    Render and Commit state

---
+++ render-commit/src/Gallery.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "./Image";
+const Gallery = () => {
+  return (
+    <section>
+      <h1>Inspiring Sculpture </h1>
+      <Image />
+      <Image />
+      <Image />
+      <Image />
+      <Image />
+    </section>
+  );
+};
+
+export default Gallery;
---
+++ render-commit/src/Image.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Image = () => {
+  return (
+    <img
+      src="https://i.imgur.com/ZF6s192.jpg"
+      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+    />
+  );
+};
+
+export default Image;
